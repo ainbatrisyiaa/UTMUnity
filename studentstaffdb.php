d3f49d4139f323840653cc0ef356b769f5203073
kamilahsb <sitinurkamilah@graduate.utm.my>
2023-12-21 00:26:38 +0800

    third commit

--- studentstaffdb.php
+++ studentstaffdb.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'users_ad');
+define('DB_USERNAME', 'DevGenius');
+define('DB_PASSWORD', 'UTMUnity67');
+define('DB_NAME', 'devgenius');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
