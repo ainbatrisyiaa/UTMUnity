674cf42494aadbc41101da04b6174b336a768425
Ain Norazlan <147370423+ainbatrisyiaa@users.noreply.github.com>
2023-12-21 01:18:45 +0800

    Commited

--- thank_you.php
+++ thank_you.php
@@ -61,7 +61,7 @@
         <h2>Thank You for Your Contribution</h2>
         <p class="thank-you-text"><i>Alone we can do so little; together we can do so much.</i></p>
         <p class="thank-you-text"><strong>                                  – Helen Keller</strong></p>
-        <a href="http://localhost/donate/main_page.php" class="back-to-home">Back to Home</a>
+        <a href="welcome.php" class="back-to-home">Back to Home</a>
     </div>
 </body>
 
