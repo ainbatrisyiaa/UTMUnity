76b991f0327f7bc682d56e12ac7a7b9ab4441e31
Ain Norazlan <147370423+ainbatrisyiaa@users.noreply.github.com>
2023-12-26 18:43:10 +0800

    Commited

--- orgzdonate.php
+++ orgzdonate.php
@@ -1,40 +1,91 @@
-<?php
-// Establish a database connection
-$servername = "localhost";
-$username = "localhost";
-$password = "donationform";
-$dbname = "donationdetails";
+<!DOCTYPE HTML>
+<html lang="en">
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Organization Donation</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #a3e4d7;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            height: 100vh;
+        }
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+        .admin-container {
+            width: 80%;
+            max-width: 600px;
+            background-color: #ffffff;
+            border-radius: 10px;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-top: 20px;
+            text-align: center;
+        }
 
-// Specify the event name you want to retrieve
-$eventName = 'Save Animals Life'; // Replace with the actual event name you want
+        .total-donation {
+            font-size: 20px;
+            font-weight: bold;
+            margin-bottom: 20px;
+        }
 
-// Fetch event data for a specific event
-$result = $conn->query("
-    SELECT UserDetails.event_name, UserDetails.donate
-    FROM UserDetails
-    WHERE UserDetails.event_name = '$eventName'
-");
+        .no-event {
+            color: #555;
+        }
+    </style>
+</head>
 
-if ($result->num_rows > 0) {
-    $totalDonation = 0;
+<body>
+    <div class="admin-container">
+        <h2>Your Total Donation</h2>
 
-    while ($row = $result->fetch_assoc()) {
-        $totalDonation += $row["donate"];
-    }
+        <?php
+        // Establish a database connection
+        $servername = "localhost";
+        $username = "localhost";
+        $password = "donationform";
+        $dbname = "donationdetails";
 
-    echo "<tr>";
-    echo "<td>" . $eventName . "</td>";
-    echo "<td>$" . number_format($totalDonation, 2) . "</td>";
-    echo "</tr>";
-} else {
-    echo "<tr><td colspan='2'>Event not found</td></tr>";
-}
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-$conn->close();
-?>
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        // Specify the event name you want to retrieve
+        $eventName = 'Save Animals Life'; // Replace with the actual event name you want
+
+        // Fetch event data for a specific event
+        $result = $conn->query("
+            SELECT UserDetails.event_name, UserDetails.donate
+            FROM UserDetails
+            WHERE UserDetails.event_name = '$eventName'
+        ");
+
+        if ($result->num_rows > 0) {
+            $totalDonation = 0;
+
+            while ($row = $result->fetch_assoc()) {
+                $totalDonation += $row["donate"];
+            }
+
+            echo "<div class='total-donation'>";
+            echo "<p>Event Name: " . $eventName . "</p>";
+            echo "<p>Total Donation: $" . number_format($totalDonation, 2) . "</p>";
+            echo "</div>";
+        } else {
+            echo "<p class='no-event'>Event not found</p>";
+        }
+
+        $conn->close();
+        ?>
+
+    </div>
+</body>
+
+</html>
