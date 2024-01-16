c15e1148a66d0d91299885674f89c5d8a4a2f5b4
kamilahsb <sitinurkamilah@graduate.utm.my>
2024-01-16 17:50:09 +0800

    committed

--- orgzprofile.php
+++ orgzprofile.php
@@ -7,7 +7,7 @@ session_start();
 
 // Check if the user is logged in
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-    header("location: userlogin.php");
+    header("location: googlelogin.php");
     exit;
 }
 
